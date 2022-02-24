7393d0ccf928c1b8ee045e1cdb347d4dd89700de
NikZhukov2505 <unnormaldj@gmail.com>
2022-02-24 16:43:52 +0600

    change styles

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -1,5 +1,4 @@
 import React from "react"
-// import './Spinner.css'
 import spinner from './../../Images/load.gif'
 
 const Spinner = () => <img src={spinner} style={{ position: 'absolute', top: '50%', left: '50%', width: '50px', height: '50px', transform: 'translate(-50%, -50%)' }} alt="Loading..." />
